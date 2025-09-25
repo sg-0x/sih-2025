a087f7e3be231486c079e1cb09dcdc71f891f0b4
sg-0x <shashwat.gaur1001@gmail.com>
2025-09-25 05:44:05 +0530

    clean

--- start-backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Backend Server...
-echo.
-echo Make sure you have Node.js installed and dependencies installed.
-echo Run 'npm install' in the backend directory if you haven't already.
-echo.
-cd backend
-node server.js
-pause
